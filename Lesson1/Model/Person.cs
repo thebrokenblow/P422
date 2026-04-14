953e9b31a12117fe1aa7c0953ec4ada5bb441a7d
Артём Красов <artkrasov@gmail.com>
2026-04-14 18:17:34 +0300

    Давил свойство Age

--- Lesson1/Model/Person.cs
+++ Lesson1/Model/Person.cs
@@ -5,4 +5,5 @@ public class Person
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Patronymic { get; set; }
+    public int Age { get; set; }
 }
