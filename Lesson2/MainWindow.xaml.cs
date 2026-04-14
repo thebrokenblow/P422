faa4ee4732bc153f3215bc28b9ef22bd9c648776
Артём Красов <artkrasov@gmail.com>
2026-04-14 19:37:06 +0300

    Добавил второе занятие

---
+++ Lesson2/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lesson2;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private string _correctFirstAnswer = "Н.М. Карамзин";
+    private string _correctSecondAnswer = "Началом промышленного переворота";
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void CheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+        if (sender is not CheckBox checkBox)
+        {
+            return;
+        }
+
+        PatronymicTextFiled.Text = string.Empty;
+
+        if (!checkBox.IsChecked.HasValue)
+        {
+            return;
+        }
+
+        PatronymicTextFiled.IsEnabled = !checkBox.IsChecked.Value;
+    }
+
+
+    private void RadioButton_FirstQuestion_Checked(object sender, RoutedEventArgs e)
+    {
+        if (sender is not RadioButton radioButton)
+        {
+            return;
+        }
+
+        var result = radioButton.Content;
+
+        if (_correctFirstAnswer.Equals(result))
+        {
+            MessageBox.Show("Правильно");
+        }
+    }
+
+    private void RadioButton_SecondQuestion_Checked(object sender, RoutedEventArgs e)
+    {
+        if (sender is not RadioButton radioButton)
+        {
+            return;
+        }
+
+        var result = radioButton.Content;
+
+        if (_correctSecondAnswer.Equals(result))
+        {
+            MessageBox.Show("Правильно");
+        }
+    }
+}
---
+++ Lesson2/Question.cs
@@ -0,0 +1,27 @@
+﻿namespace Lesson2;
+
+public class Question
+{
+    public string TextQuestion { get; }
+
+    private Dictionary<string, bool> _isCorrectByAnswer;
+
+    public Question(string textQuestion, Dictionary<string, bool> isCorrectByAnswer)
+    {
+        TextQuestion = textQuestion;
+        _isCorrectByAnswer = isCorrectByAnswer;
+    }
+
+    public bool IsCorrectByAnswer(List<string> answer)
+    {
+        for (int i = 0; i < answer.Count; i++)
+        {
+            if (!_isCorrectByAnswer[answer[i]])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
