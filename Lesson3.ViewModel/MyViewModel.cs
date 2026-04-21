5c23b5fd02507c43ca81b07094ada2b43de82fe3
Артём Красов <artkrasov@gmail.com>
2026-04-21 19:56:24 +0300

    Добавил третье занятие

---
+++ Lesson3.ViewModel/MyViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Lesson3.ViewModel;
+
+public class MyViewModel
+{
+    private string firstName;
+
+    public string FirstName
+    {
+        get
+        {
+            return firstName;
+        }
+        set 
+        {
+            firstName = value;
+        }
+    }
+
+    private string lastName;
+
+    public string LastName
+    {
+        get
+        {
+            return lastName;
+        }
+        set
+        {
+            lastName = value;
+        }
+    }
+}
---
+++ Lesson3/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Lesson3.ViewModel;
+
+namespace Lesson3;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = new MyViewModel();
+    }
+}
