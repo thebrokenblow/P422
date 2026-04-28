c79fb975177375d06178c4c5584cdaff3e354265
Артём Красов <artkrasov@gmail.com>
2026-04-28 20:09:50 +0300

    Добавил редактирование и удаление

--- Lesson5First.ViewModel/MainViewModel.cs
+++ Lesson5First.ViewModel/MainViewModel.cs
@@ -1,18 +1,56 @@
 ﻿using Lesson5.Model;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Lesson5.ViewModel;
 
-public class MainViewModel
+public class MainViewModel : INotifyPropertyChanged
 {
     public ObservableCollection<ProductViewModel> Products { get; init; } = [];
 
+    private ProductViewModel _selectedProduct;
+    public ProductViewModel SelectedProduct
+    {
+        get => _selectedProduct;
+        set
+        {
+            _selectedProduct = value;
+            OnPropertyChanged();
+        }
+    }
+
     public string Name { get; set; }
+
+    public RelayCommand SaveEditedProductCommand { get; set; }
+    public RelayCommand DeleteProductCommand { get; set; }
 
     public MainViewModel()
     {
+        DeleteProductCommand = new(DeleteProduct);
+        SaveEditedProductCommand = new(SaveEditedProduct);
+
         var products = ProductRepository.GetProduct();
         GetProductsViewModel(products);
+    }
+
+    private void SaveEditedProduct(object? parametr)
+    {
+        SelectedProduct = null;
+    }
+
+    private void DeleteProduct(object? parametr)
+    {
+        if (parametr is not int id)
+        {
+            return;
+        }
+
+        var product = Products.FirstOrDefault(x => x.Id == id);
+        if (product != null)
+        {
+            Products.Remove(product);
+        }
     }
 
     private void GetProductsViewModel(List<Product> products)
@@ -34,5 +72,11 @@ public class MainViewModel
     public void AddProduct()
     {
         
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string prop = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
     }
 }
---
+++ Lesson5First.ViewModel/RelayCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace Lesson5.ViewModel;
+
+public class RelayCommand : ICommand
+{
+    private Action<object?> execute;
+    private Func<object?, bool>? canExecute;
+
+    public event EventHandler CanExecuteChanged;
+    public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+    {
+        this.execute = execute;
+        this.canExecute = canExecute;
+    }
+
+    public void Execute(object? parameter)
+    {
+        execute.Invoke(parameter);
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+}
