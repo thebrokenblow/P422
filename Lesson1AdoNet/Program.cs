058dc7a140696347d298312d9b606d2d6567add5
Артём Красов <artkrasov@gmail.com>
2026-04-30 20:38:00 +0300

    Добавил первое занятие по Ado net

---
+++ Lesson1AdoNet/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+
+var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=P422Shop;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30";
+var category = Console.ReadLine();
+
+try
+{
+    using var connection = new SqlConnection(connectionString);
+    connection.Open();
+
+    var sqlQuery = @"select g.id, g.name, c.name from goods as g
+                     join categories as c on c.id = g.id_category
+                     where c.name = @category";
+
+    using var sqlCommand = new SqlCommand(sqlQuery, connection);
+
+    var categoryParam = new SqlParameter("@category", category);
+    sqlCommand.Parameters.Add(categoryParam);
+
+    var sqlDataReader = sqlCommand.ExecuteReader();
+    if (sqlDataReader.HasRows)
+    {
+        var name1 = sqlDataReader.GetName(0);
+        var name2 = sqlDataReader.GetName(1);
+        var name3 = sqlDataReader.GetName(2);
+
+        while (sqlDataReader.Read())
+        {
+            var value1 = sqlDataReader.GetInt32(0);
+            var value2 = sqlDataReader.GetString(1);
+            var value3 = sqlDataReader.GetString(2);
+
+            Console.WriteLine($"{name1}: {value1}, {name2}: {value2}, {name3}: {value3}");
+        }
+    }
+}
+catch
+{
+    
+}
--- Lesson5First.Model/ProductRepository.cs
+++ Lesson5First.Model/ProductRepository.cs
@@ -1,15 +1,47 @@
-﻿namespace Lesson5.Model;
+﻿using Microsoft.Data.SqlClient;
+
+namespace Lesson5.Model;
 
 public class ProductRepository
 {
     public static List<Product> GetProduct()
     {
-        return
-        [
-            new() { Id = 1, Name = "Ноутбук", Category = "Электроника", Price = 45000 },
-            new() { Id = 2, Name = "Мышь", Category = "Электроника", Price = 1500 },
-            new() { Id = 3, Name = "Книга", Category = "Обучение", Price = 800 },
-            new() { Id = 4, Name = "Ручка", Category = "Канцтовары", Price = 50 }
-        ];
+        var products = new List<Product>();
+        var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=P422Shop;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30";
+
+        try
+        {
+ 
+            string password = "'' or 1 = 1";
+
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            var sqlQuery = @$"select * from users where password = {password}";
+
+            using var sqlCommand = new SqlCommand(sqlQuery, connection);
+            var sqlDataReader = sqlCommand.ExecuteReader();
+            if (sqlDataReader.HasRows)
+            {
+
+                while (sqlDataReader.Read())
+                {
+                    var product = new Product
+                    {
+                        Id = sqlDataReader.GetInt32(0),
+                        Name = sqlDataReader.GetString(1),
+                        Category = sqlDataReader.GetString(2),
+                        Price = sqlDataReader.GetDecimal(3),
+                    };
+                    products.Add(product);
+                }
+            }
+        }
+        catch
+        {
+
+        }
+
+        return products;
     }
 }
